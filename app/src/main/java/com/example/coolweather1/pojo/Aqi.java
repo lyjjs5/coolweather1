3932367fe0bbdd545fb8fa0c52e72b5ad40f0931
lyjjs5 <825275741@qq.com>
2018-01-02 15:35:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/coolweather1/pojo/Aqi.java
@@ -0,0 +1,22 @@
+/**
+  * Copyright 2018 bejson.com 
+  */
+package com.example.coolweather1.pojo;
+
+/**
+ * Auto-generated: 2018-01-02 10:50:33
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Aqi {
+
+    private City city;
+    public void setCity(City city) {
+         this.city = city;
+     }
+     public City getCity() {
+         return city;
+     }
+
+}
---
+++ app/src/main/java/com/example/coolweather1/pojo/Tmp.java
@@ -0,0 +1,30 @@
+/**
+  * Copyright 2018 bejson.com 
+  */
+package com.example.coolweather1.pojo;
+
+/**
+ * Auto-generated: 2018-01-02 10:50:33
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Tmp {
+
+    private String max;
+    private String min;
+    public void setMax(String max) {
+         this.max = max;
+     }
+     public String getMax() {
+         return max;
+     }
+
+    public void setMin(String min) {
+         this.min = min;
+     }
+     public String getMin() {
+         return min;
+     }
+
+}
